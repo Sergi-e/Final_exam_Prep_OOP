2a91dbbea8aad7557e1403dd39f8da6469c5f035
sergi-e <ishserge16@gmail.com>
2025-12-10 14:46:35 +0000

    Student class created

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student extends Person{
+    private double[] grades;
+    private static final double PASSING_GRADE=50.0;
+    Student(String firstName, String lastName, int id, double[] grades){
+    super(firstName, lastName, id);
+    this.grades=grades;
+    system.out.print(id);
+    }
+}
+
+
