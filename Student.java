1cc03beb1743eff8dd0d0c53141ddc7104a32497
sergi-e <ishserge16@gmail.com>
2025-12-10 23:28:48 +0000

    Updated the student class

--- Student.java
+++ Student.java
@@ -1,29 +1,36 @@
-public class Student extends Person{
+class Student extends Person{
     
     private double[] grades;
     private static final double PASSING_GRADE=50.0;
     
     //constructor 1
     public Student(String firstName, String lastName, int id, double[] grades){
-     super(firstName, lastName, id);
-    this.grades=grades;
-    System.out.print(id);
+    
+        super(firstName, lastName, id);
+        this.grades= new double[grades.length];
+        for(int i=0;  i< grades.length; i++){
+            this.grades[i]=grades[i];
+        }
+        System.out.println("Student constructor called for " + id);
+    }
+    
+    //implementing the role class
+    @Override
+    public String role(){
+        return "Student";
     }
 
-    public double average(){
-        double aver=0;
-        double sum=0;
-        int l=grades.length;
-        
-        for(int i=0; i < l; i++){
+    public double average(){  
+        if (grades.length==0) return 0;
+            double sum=0;
+        for(int i=0; i < grades.length; i++){
             sum+=grades[i];
-            aver=sum/l;
         }
-        return aver;
+        return sum/grades.length;
     }
     
     public boolean hasPassed(){
-        if(average()<PASSING_GRADE){
+        if(average()< PASSING_GRADE){
             return false;
         }
         return true;
