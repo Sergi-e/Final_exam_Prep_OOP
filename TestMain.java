65cdf01c35e7adcf620b6f665d34d403dbc62e87
sergi-e <ishserge16@gmail.com>
2025-12-10 22:12:13 +0000

    Created the test class for testing

---
+++ TestMain.java
@@ -0,0 +1,13 @@
+public class TestMain {
+    public static void main(String[] args) {
+        Student stud1= new Student("Shimo", "Brais", 47584, new double[]{4,5,6,5});
+        Student stud2= new Student("Kagira", "Egide", 5575, new double[]{} );
+        Teacher teach1=new Teacher("Faustin", "Ishimwe", 6748, "OOP");
+
+        Person people[]={stud1, stud2, teach1};
+
+        //displays and conditions
+
+    }
+    
+}
