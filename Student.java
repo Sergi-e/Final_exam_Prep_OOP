e1b8e73f61f6a249d2264ce2a58ec89e48785546
sergi-e <ishserge16@gmail.com>
2025-12-10 22:11:42 +0000

    Made some changes

--- Student.java
+++ Student.java
@@ -1,10 +1,32 @@
 public class Student extends Person{
+    
     private double[] grades;
     private static final double PASSING_GRADE=50.0;
-    Student(String firstName, String lastName, int id, double[] grades){
-    super(firstName, lastName, id);
+    
+    //constructor 1
+    public Student(String firstName, String lastName, int id, double[] grades){
+     super(firstName, lastName, id);
     this.grades=grades;
-    system.out.print(id);
+    System.out.print(id);
+    }
+
+    public double average(){
+        double aver=0;
+        double sum=0;
+        int l=grades.length;
+        
+        for(int i=0; i < l; i++){
+            sum+=grades[i];
+            aver=sum/l;
+        }
+        return aver;
+    }
+    
+    public boolean hasPassed(){
+        if(average()<PASSING_GRADE){
+            return false;
+        }
+        return true;
     }
 }
 
