b937a28810ad0dd02de02322e920c187e316d747
sergi-e <ishserge16@gmail.com>
2025-12-11 15:41:06 +0000

    Implemented the Project classes requirements

--- TestMain.java
+++ TestMain.java
@@ -1,10 +1,14 @@
 public class TestMain {
     public static void main(String[] args) {
         Student stud1= new Student("Shimo", "Brais", 47584, new double[]{4,5,6,5});
-        Student stud2= new Student("Kagira", "Egide", 5575, new double[]{} );
+        Student stud2= new Student("Kagira", "Egide", 5575, new double[]{5, 5, 6, 7, 6} );
         Teacher teach1=new Teacher("Faustin", "Ishimwe", 6748, "OOP");
 
         Person people[]={stud1, stud2, teach1};
+
+        //objects for the project
+        Project pro1=new Project("Ecofind", new double[]{5,3,5,6,4});
+        Project pro2=new Project("KivuSpeaks", new double[]{6,7,8,9});
 
         //displays and conditions
         for(Person p: people){
@@ -27,5 +31,8 @@ public class TestMain {
                 System.out.println("Subject: " + t.getSubject());
             }
         }
+          Person[][] people;
+        
+
     }
 }
